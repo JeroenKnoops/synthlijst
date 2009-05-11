d44b520df32ff179761b0adda7c3b5ff8d7441eb
Jeroen Knoops <jeroen.knoops@gmail.com>
2009-05-11 21:45:16 +0200

    Added factory in synthesizer specs.

--- spec/models/synthesizer_spec.rb
+++ spec/models/synthesizer_spec.rb
@@ -1,18 +1,20 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Synthesizer do
-  before(:each) do
-    @valid_attributes = {
-      :title => "value for title",
-      :description => "value for description"
-    }
-  end
+  # before(:each) do
+  #   @valid_attributes = {
+  #     :title => "value for title",
+  #     :description => "value for description"
+  #   }
+  # end
 
-  it "should create a new instance given valid attributes" do
-    Synthesizer.create!(@valid_attributes)
+  it "should test synthesizer factory" do
+    synthesizer = Factory.build(:synthesizer)
+    synthesizer.should be_valid
   end
   
-  it "should not create a new instance since title is not given" do
-    Synthesizer.create(:description => "a nameless synthesizer").save.should be_false
+  it "should not be valid without title" do
+    synthesizer = Factory.build(:synthesizer, {:title => nil})
+    synthesizer.should_not be_valid
   end
 end
